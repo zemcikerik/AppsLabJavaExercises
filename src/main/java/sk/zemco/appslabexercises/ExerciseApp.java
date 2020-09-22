862f129355bec4c8852b8ed785594ceb8e663305
Erik Zemčík <zemcik.erik@gmail.com>
2020-09-22 16:56:47 +0200

    Created the project

---
+++ src/main/java/sk/zemco/appslabexercises/ExerciseApp.java
@@ -0,0 +1,12 @@
+package sk.zemco.appslabexercises;
+
+public final class ExerciseApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Apps Lab!");
+    }
+
+    // don't allow instantiating of this class
+    private ExerciseApp() { }
+
+}
